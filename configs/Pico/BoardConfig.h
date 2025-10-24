f4044310bbea92978d5e1b28d3071ddcb4804c2c
cumings12 <92919004+cumings12@users.noreply.github.com>
2025-10-24 19:59:06 +1100

    Update BoardConfig.h

--- configs/Pico/BoardConfig.h
+++ configs/Pico/BoardConfig.h
@@ -17,12 +17,12 @@
 #define GPIO_PIN_06 GpioAction::BUTTON_PRESS_DOWN   // DOWN   | DOWN   | DOWN    | DOWN     | DOWN   | DOWN   |
 #define GPIO_PIN_07 GpioAction::BUTTON_PRESS_RIGHT  // RIGHT  | RIGHT  | RIGHT   | RIGHT    | RIGHT  | RIGHT  |
 #define GPIO_PIN_05 GpioAction::BUTTON_PRESS_LEFT   // LEFT   | LEFT   | LEFT    | LEFT     | LEFT   | LEFT   |
-#define GPIO_PIN_19 GpioAction::BUTTON_PRESS_B1     // B1     | A      | B       | Cross    | 2      | K1     |
-#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_B2     // B2     | B      | A       | Circle   | 3      | K2     |
+#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_B1     // B1     | A      | B       | Cross    | 2      | K1     |
+#define GPIO_PIN_18 GpioAction::BUTTON_PRESS_B2     // B2     | B      | A       | Circle   | 3      | K2     |
 #define GPIO_PIN_27 GpioAction::BUTTON_PRESS_R2     // R2     | RT     | ZR      | R2       | 8      | K3     |
 #define GPIO_PIN_26 GpioAction::BUTTON_PRESS_L2     // L2     | LT     | ZL      | L2       | 7      | K4     |
-#define GPIO_PIN_18 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
-#define GPIO_PIN_20 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
+#define GPIO_PIN_21 GpioAction::BUTTON_PRESS_B3     // B3     | X      | Y       | Square   | 1      | P1     |
+#define GPIO_PIN_19 GpioAction::BUTTON_PRESS_B4     // B4     | Y      | X       | Triangle | 4      | P2     |
 #define GPIO_PIN_11 GpioAction::BUTTON_PRESS_R1     // R1     | RB     | R       | R1       | 6      | P3     |
 #define GPIO_PIN_15 GpioAction::BUTTON_PRESS_L1     // L1     | LB     | L       | L1       | 5      | P4     |
 #define GPIO_PIN_28 GpioAction::BUTTON_PRESS_S2     // S2     | Start  | Plus    | Start    | 10     | Start  |
@@ -60,14 +60,14 @@
  
 #define USB_PERIPHERAL_ENABLED 1
 
-#define USB_PERIPHERAL_PIN_DPLUS 12
-#define USB_PERIPHERAL_PIN_ORDER 0
+#define USB_PERIPHERAL_PIN_DPLUS 13
+#define USB_PERIPHERAL_PIN_ORDER 1
  
  #define DEFAULT_INPUT_MODE_R1 INPUT_MODE_XBONE
  #define DEFAULT_INPUT_MODE_B4 INPUT_MODE_PS5
  #define DEFAULT_PS5AUTHENTICATION_TYPE INPUT_MODE_AUTH_TYPE_USB
  
- #define TURBO_ENABLED 1
+ #define TURBO_ENABLED 0
  #define GPIO_PIN_09 GpioAction::BUTTON_PRESS_TURBO
  
  #define BOARD_LEDS_PIN 29
@@ -104,10 +104,10 @@
  #define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_BOARD_DEFINED_B
  
 #define DEFAULT_BOARD_LAYOUT_A {\
-    {GP_ELEMENT_PIN_BUTTON, {95,  13, 4, 4, 1, 1, 4,   GP_SHAPE_ELLIPSE}},\
-    {GP_ELEMENT_PIN_BUTTON, {114,  15, 4, 4, 1, 1, 7,    GP_SHAPE_ELLIPSE}},\
-    {GP_ELEMENT_PIN_BUTTON, {103,  23, 4, 4, 1, 1, 6,    GP_SHAPE_ELLIPSE}},\
-    {GP_ELEMENT_PIN_BUTTON, {91,  32, 4, 4, 1, 1, 5,    GP_SHAPE_ELLIPSE}},\
+    {GP_ELEMENT_PIN_BUTTON, {95,  12, 4, 4, 1, 1, 4,   GP_SHAPE_ELLIPSE}},\
+    {GP_ELEMENT_PIN_BUTTON, {114,  14, 4, 4, 1, 1, 7,    GP_SHAPE_ELLIPSE}},\
+    {GP_ELEMENT_PIN_BUTTON, {103,  22, 4, 4, 1, 1, 6,    GP_SHAPE_ELLIPSE}},\
+    {GP_ELEMENT_PIN_BUTTON, {92,  32, 4, 4, 1, 1, 5,    GP_SHAPE_ELLIPSE}},\
     {GP_ELEMENT_PIN_BUTTON, {93,  45, 4, 4, 1, 1, 11,   GP_SHAPE_ELLIPSE}},\
     {GP_ELEMENT_PIN_BUTTON, {82,  56, 4, 5, 1, 1, 15,    GP_SHAPE_ELLIPSE}},\
     {GP_ELEMENT_PIN_BUTTON, {65,  55, 4, 4, 1, 1, 16,   GP_SHAPE_ELLIPSE}},\
